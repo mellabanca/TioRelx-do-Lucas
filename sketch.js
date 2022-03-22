772514be0687d8f34357985c3dc041839206a788
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-03-22 11:26:02 -0300

    22/03/2022

--- sketch.js
+++ sketch.js
@@ -1,27 +1,28 @@
 var tiorelx, fugindo;
-
+var terra;
+var imagempiso
 function preload(){
-  fugindo = loadAnimation("trex1.png","trex3.png","trex4.png");
-
+fugindo = loadAnimation("trex1.png","trex3.png","trex4.png");
+imagempiso=loadImage("ground2.png")
 }
-
 function setup(){
-  createCanvas(1200,400);
-  tiorelx = createSprite(100,320,40,100);
-  tiorelx.addAnimation("fugindo",fugindo);
-  borda = createEdgeSprites();
- 
+createCanvas(1200,400);
+tiorelx = createSprite(100,320,40,100);
+tiorelx.addAnimation("fugindo",fugindo);
+borda = createEdgeSprites();
+terra=createSprite(400,360,800,40);
+terra.addImage(imagempiso);
+terra.x=terra.width/2
 }
-
 function draw(){
-  background("lightgrey");
-
-  if(keyDown("space")){
-    tiorelx.velocityY = -20;
-  }
-  tiorelx.velocityY = tiorelx.velocityY + 2;
-
-  tiorelx.collide(borda[3]);
-  
-  drawSprites();
+background("lightgrey");
+if(keyDown("space")){
+tiorelx.velocityY = -20;
+}
+tiorelx.velocityY = tiorelx.velocityY + 2;
+terra.velocityX=-4;
+if(terra.x<0)
+{terra.x=terra.width/2};
+tiorelx.collide(terra);
+drawSprites();
 }
